ae5bca444524a01c21dc7b94c48972bfd7356b2f
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-05-02 20:47:50 -0400

    fix mobile device detection

--- main.js
+++ main.js
@@ -55,13 +55,14 @@ window.onclick = function(event) {
     }
 }
 
-// redirect to mobile page if user is on mobile device
-let isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
-if (typeof screen.orientation !== 'undefined' || isMac) {
-    // not mobile
-} else {
-    // mobile
+if (navigator.userAgent.match(/Android/i)
+|| navigator.userAgent.match(/webOS/i)
+|| navigator.userAgent.match(/iPhone/i)
+|| navigator.userAgent.match(/iPad/i)
+|| navigator.userAgent.match(/iPod/i)
+|| navigator.userAgent.match(/BlackBerry/i)
+|| navigator.userAgent.match(/Windows Phone/i)) {
     if (!location.pathname.startsWith('/mobile')) {
-        window.open('/mobile/index', '_self');
+        window.open('/mobile', '_self');
     }
 }
