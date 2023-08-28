fa79a3149b1236ba8f61d3ff021413dd2accae0a
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-08-28 09:35:29 -0400

    more secure auth token handling

--- main.js
+++ main.js
@@ -1,36 +1,19 @@
 const liveServer = 'https://api.flashstudy.org/v1';
-const localServer = 'http://localhost:5001';
+const localServer = 'http://localhost:5001/v1';
 const activeServer = location.host.startsWith('localhost') || location.host.startsWith('127.0.0.1')  ? localServer : liveServer;
-
-let cookies = document.cookie.split(';');
-let jwt;
-
-for (cookie in cookies) {
-    if (cookies[cookie].trim().startsWith('jwt')) {
-        jwt = cookies[cookie].trim().slice(4);
-    }
-}
 
 function fetcher(endpoint, options) {
     let updatedOptions = { ...options };
 
-    if (jwt) {
-        updatedOptions.headers = {
-            ...options ? options.headers : null,
-            'x-access-token': jwt,
-            // not sure if this actually works as expected -- hopefully reduces preflight requests?
-            'Access-Control-Max-Age': 86400
-        }
+    updatedOptions.headers = {
+        ...options ? options.headers : null,
     }
+
     return fetch(`${activeServer}${endpoint}`, updatedOptions)
 }
 
-function logout() {
-    //remove jwt from user storage
-    document.cookie = 'jwt=';
-
-    //log out user on backend
-    // ...
+async function logout() {
+    await fetcher('/auth/logout')
     
     //reload page
     location.reload();
